be75ac5b48c3e51e98522792e5c426292929fbaa
DoNguyenHoangHai <hai.do@topdev.vn>
2025-06-14 00:47:06 +0700

    Implement elasticsearch

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -4,13 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Brand extends Model
 {
     use HasFactory;
+    use Searchable;
 
     protected $fillable = [
         'name',
         'created_by',
     ];
+
+    public function toSearchableArray(): array
+    {
+        return [
+            'name'          => $this->name,
+            'created_by'    => $this->created_by
+        ];
+    }
+
+    public function searchableAs(): string
+    {
+        return 'brand_index';
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Psr\Http\Client\ClientInterface;
+use Http\Adapter\Guzzle7\Client as GuzzleAdapter;
+use Elastic\Elasticsearch\Client;
+use Elastic\Elasticsearch\ClientBuilder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +15,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // BIND PSR-18 HTTP client
+        $this->app->bind(ClientInterface::class, function () {
+            return new GuzzleAdapter(new \GuzzleHttp\Client());
+        });
+
+        // BIND NodePoolInterface (Elasticsearch v8+ yêu cầu)
+        $this->app->singleton(Client::class, function () {
+            return ClientBuilder::create()
+                ->setHosts([env('ELASTICSEARCH_HOST', 'http://localhost:9200')])
+                ->build();
+        });
     }
 
     /**
